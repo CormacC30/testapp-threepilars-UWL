3ee8399023c46d870952baee48141d95d33de8e9
Nigel Smith <nigsmith@redhat.com>
2026-03-03 17:31:50 +0000

    update deploy script

--- 00_Deploy.sh
+++ 00_Deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#Install Odf
+00_Install_Odf/00_preInstall.sh
+oc create -Rf 00_Install_Odf/01_subscription_odf.yaml
+oc create -Rf 00_Install_Odf/02_storagecluster.yaml
+00_Install_Odf/03_postInstall.sh
+
 #Operators
 oc create -Rf 01_Operators/
 
@@ -17,7 +23,7 @@ oc create -f 03_Logging/05_alertingrule.yaml
 oc create -f 04_Opentelemetry/01_collector.yaml  
 
 #Tempo
-oc create -f 05_Tempo/
+oc create -f 05_Tempo/01_objectclaim.yaml
 05_Tempo/02_bucketsecret.sh
 oc create -f 05_Tempo/03_tempo.yaml
 co create -f 05_Tempo/04_uiplugin
